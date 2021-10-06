e7e77111d82eb94f036dc7c4a90c08f2e96d8d8d
Victor Mello <victorsmello93@gmail.com>
2021-10-05 21:57:00 -0300

    Configurando ESLint e Prettier

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,5 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-	return res.json({message: "Hello World"});
-});
-
-app.listen(3000, () => console.log("Server is running"));
+app.listen(3333, () => console.log('Server is running!'));
