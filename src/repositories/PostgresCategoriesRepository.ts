a8764eb20cee80c1717fbc4b11e547718a7475ae
Victor Mello <victorsmello93@gmail.com>
2021-10-12 16:10:07 -0300

    Aplicando DIP e LSP para criar uma nova classe concreta que também implementa a interface ICategoriesRepository

---
+++ src/repositories/PostgresCategoriesRepository.ts
@@ -0,0 +1,20 @@
+import { Category } from '../models/Category';
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from './ICategoriesRepository';
+
+class PostgresCategoriesRepository implements ICategoriesRepository {
+  create({ name, description }: ICreateCategoryDTO): void {
+    console.log(name, description);
+  }
+  list(): Category[] {
+    return null;
+  }
+  findByName(name: string): Category {
+    console.log(name);
+    return null;
+  }
+}
+
+export { PostgresCategoriesRepository };
