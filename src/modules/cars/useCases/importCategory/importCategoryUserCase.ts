8f28b52accf2e065bc203a9020cbbab02747cbeb
Victor Mello <victorsmello93@gmail.com>
2021-10-13 22:05:22 -0300

    Lendo e salvando os dados do upload de arquivo

--- src/modules/cars/useCases/importCategory/importCategoryUserCase.ts
+++ src/modules/cars/useCases/importCategory/importCategoryUserCase.ts
@@ -1,18 +1,51 @@
 import fs from 'fs';
 import csvParse from 'csv-parse';
+import { ICategoriesRepository } from '../../repositories/interfaces/ICategoriesRepository';
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    // Leitura de arquivos em batch, otimizando a performance da aplicação
-    const stream = fs.createReadStream(file.path);
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    const parseFile = csvParse();
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
 
-    stream.pipe(parseFile);
+    categories.map(async category => {
+      const { name, description } = category;
 
-    // Lendo linha por linha do arquivo csv, cada uma delas representando um array de dados de categoria de carros
-    parseFile.on('data', async line => {
-      console.log(line);
+      const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+      if (!categoryAlreadyExists) {
+        this.categoriesRepository.create({ name, description });
+      }
+    });
+  }
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      // Leitura de arquivos em batch, otimizando a performance da aplicação
+      const stream = fs.createReadStream(file.path);
+
+      const categories: IImportCategory[] = [];
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      // Lendo linha por linha do arquivo csv, cada uma delas representando um array de dados de categoria de carros
+      parseFile
+        .on('data', async line => {
+          const [name, description] = line;
+          categories.push({ name, description });
+        })
+        .on('end', () => {
+          resolve(categories);
+        })
+        .on('error', err => {
+          reject(err);
+        });
     });
   }
 }
