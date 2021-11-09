efd2b17e4757f2dde42a5fb4d1a9e5fe77ec0d0b
Victor Mello <victorsmello93@gmail.com>
2021-11-09 13:16:22 -0300

    Implementação da autenticação do usuário com token JWT

---
+++ src/modules/accounts/useCases/auth/AuthenticateUserUseCase.ts
@@ -0,0 +1,52 @@
+import { injectable, inject } from 'tsyringe';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { IUsersRepository } from '../../repositories/interfaces/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    const passwordMatch = user ? await compare(password, user.password) : null;
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect');
+    }
+
+    // Gerando token JWT do usuário autenticado
+    const token = sign({}, '5f90046bbb4f2f7e9f2333d15fd0389b', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
