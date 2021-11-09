e4005db518f1a5f88839ed0f320a71f7739e72d9
Victor Mello <victorsmello93@gmail.com>
2021-11-09 08:18:43 -0300

    Removendo a coluna username de usuário pois será tomado como base o e-mail do usuário a ser cadastrado

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -5,12 +5,12 @@ import { CreateUserUseCase } from './CreateUserUseCase';
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
     try {
-      const { name, username, email, driver_license, password, avatar } = request.body;
+      const { name, email, driver_license, password, avatar } = request.body;
 
       // Injetando a dependência na classe CreateUserUseCase
       const createUserUseCase = container.resolve(CreateUserUseCase);
 
-      await createUserUseCase.execute({ name, username, email, driver_license, password, avatar });
+      await createUserUseCase.execute({ name, email, driver_license, password, avatar });
 
       return response.status(201).send();
     } catch (error) {
