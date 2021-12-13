c2da0641ce398f387bdd4de065a825254360c40d
Victor Mello <victorsmello93@gmail.com>
2021-12-13 18:44:28 -0300

    Finalizada a implementação de upload de imagens de carros

--- src/modules/cars/useCases/uploadImage/UploadCarImageUseCase.ts
+++ src/modules/cars/useCases/uploadImage/UploadCarImageUseCase.ts
@@ -1,3 +1,22 @@
-class UploadCarImageUseCase {}
+import { ICarsImagesRepository } from '@modules/cars/repositories/interfaces/ICarsImagesRepository';
+import { inject, injectable } from 'tsyringe';
+
+interface IRequest {
+  car_id: string;
+  images_name: string[];
+}
+
+@injectable()
+class UploadCarImageUseCase {
+  constructor(
+    @inject('CarsImagesRepository')
+    private carsImageRepository: ICarsImagesRepository,
+  ) {}
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
+    images_name.map(async image => {
+      await this.carsImageRepository.create(car_id, image);
+    });
+  }
+}
 
 export { UploadCarImageUseCase };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,10 @@
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/interfaces/IUsersRepository';
+import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImagesRepository';
 import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { CategoriesRepository } from '@modules/cars/infra/typeorm/repositories/CategoriesRepository';
 import { SpecificationsRepository } from '@modules/cars/infra/typeorm/repositories/SpecificationsRepository';
+import { ICarsImagesRepository } from '@modules/cars/repositories/interfaces/ICarsImagesRepository';
 import { ICarsRepository } from '@modules/cars/repositories/interfaces/ICarsRepository';
 import { ICategoriesRepository } from '@modules/cars/repositories/interfaces/ICategoriesRepository';
 import { ISpecificationsRepository } from '@modules/cars/repositories/interfaces/ISpecificationsRepository';
@@ -19,3 +21,6 @@ container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository
 
 // Injeção de dependência de carros - ICarsRepository
 container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
+
+// Injeção de dependência de upload de imagens de carros - ICarsImagesRepository
+container.registerSingleton<ICarsImagesRepository>('CarsImagesRepository', CarsImagesRepository);
