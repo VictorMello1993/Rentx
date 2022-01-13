5251ba1c409bcd3965c1b681560349d28f34f065
Victor Mello <victorsmello93@gmail.com>
2022-01-13 20:57:52 -0300

    Correções no teste CreateRentalUseCase.spec.ts

--- src/modules/rentals/usecases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/usecases/createRental/CreateRentalUseCase.spec.ts
@@ -3,35 +3,32 @@ import { RentalsRepositoryFake } from '@modules/rentals/repositories/fakes/Renta
 import { AppError } from '@shared/errors/AppError';
 import { DayjsDateProvider } from '@shared/container/providers/DateProvider/implementations/DayjsDateProvider';
 import { CarsRepositoryFake } from '@modules/cars/repositories/fakes/CarsRepositoryFake';
-import { CreateCarUseCase } from '@modules/cars/useCases/createCar/CreateCarUseCase';
 import { CreateRentalUseCase } from './CreateRentalUseCase';
 
 let createRentalUseCase: CreateRentalUseCase;
 let rentalsRepositoryFake: RentalsRepositoryFake;
 let dayJsProvider: DayjsDateProvider;
 let carsRepositoryFake: CarsRepositoryFake;
-let createCarUseCase: CreateCarUseCase;
 
 describe('Create rental', () => {
   const dayAdd24Hours = dayjs().add(1, 'day').toDate();
 
   beforeEach(() => {
-    rentalsRepositoryFake = new RentalsRepositoryFake();
     carsRepositoryFake = new CarsRepositoryFake();
+    rentalsRepositoryFake = new RentalsRepositoryFake();
     dayJsProvider = new DayjsDateProvider();
     createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryFake, dayJsProvider, carsRepositoryFake);
-    createCarUseCase = new CreateCarUseCase(carsRepositoryFake);
   });
 
   it('Should be able to create a new rental', async () => {
-    const car = await createCarUseCase.execute({
-      name: 'Car1',
-      description: 'Car test',
-      daily_date: 350,
-      license_plate: 'AAA-756',
-      fine_amount: 200,
-      brand: 'Brand',
-      category_id: 'blabla',
+    const car = await carsRepositoryFake.create({
+      name: 'Test',
+      description: 'Car Test',
+      daily_date: 100,
+      license_plate: 'test',
+      fine_amount: 40,
+      category_id: '1234',
+      brand: 'brand',
     });
 
     const rental = await createRentalUseCase.execute({
@@ -45,18 +42,18 @@ describe('Create rental', () => {
   });
 
   it("It should not be able to create a new rental if there's another open to the same user", async () => {
-    expect(async () => {
-      const car = await createCarUseCase.execute({
-        name: 'Car1',
-        description: 'Car test',
-        daily_date: 350,
-        license_plate: 'AAA-756',
-        fine_amount: 200,
-        brand: 'Brand',
-        category_id: 'blabla',
+    await expect(async () => {
+      const car = await carsRepositoryFake.create({
+        name: 'Test',
+        description: 'Car Test',
+        daily_date: 100,
+        license_plate: 'test',
+        fine_amount: 40,
+        category_id: '1234',
+        brand: 'brand',
       });
 
-      await createRentalUseCase.execute({
+      await rentalsRepositoryFake.create({
         user_id: '123',
         car_id: car.id,
         expected_return_date: dayAdd24Hours,
@@ -64,22 +61,22 @@ describe('Create rental', () => {
 
       await createRentalUseCase.execute({
         user_id: '123',
-        car_id: 'aaaaa',
+        car_id: car.id,
         expected_return_date: dayAdd24Hours,
       });
     }).rejects.toBeInstanceOf(AppError);
   });
 
   it("It should not be able to create a new rental if there's another open to the same car", async () => {
-    expect(async () => {
-      const car = await createCarUseCase.execute({
-        name: 'Car1',
-        description: 'Car test',
-        daily_date: 350,
-        license_plate: 'AAA-756',
-        fine_amount: 200,
-        brand: 'Brand',
-        category_id: 'blabla',
+    await expect(async () => {
+      const car = await carsRepositoryFake.create({
+        name: 'Test',
+        description: 'Car Test',
+        daily_date: 100,
+        license_plate: 'test',
+        fine_amount: 40,
+        category_id: '1234',
+        brand: 'brand',
       });
 
       await createRentalUseCase.execute({
@@ -97,15 +94,15 @@ describe('Create rental', () => {
   });
 
   it('It should not be able to create a new rental with invalid return time', async () => {
-    expect(async () => {
-      const car = await createCarUseCase.execute({
-        name: 'Car1',
-        description: 'Car test',
-        daily_date: 350,
-        license_plate: 'AAA-756',
-        fine_amount: 200,
-        brand: 'Brand',
-        category_id: 'blabla',
+    await expect(async () => {
+      const car = await carsRepositoryFake.create({
+        name: 'Test',
+        description: 'Car Test',
+        daily_date: 100,
+        license_plate: 'test',
+        fine_amount: 40,
+        category_id: '1234',
+        brand: 'brand',
       });
 
       await createRentalUseCase.execute({
