e564025026cd6ef2dfa714da7f878a869d92c00f
Victor Mello <victorsmello93@gmail.com>
2022-01-20 10:12:52 -0300

    Implementado alteração de senha

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,10 +1,13 @@
 import { ForgotPasswordMailController } from '@modules/accounts/useCases/forgotPasswordMail/ForgotPasswordMailController';
+import { ResetPasswordUserController } from '@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController';
 import { Router } from 'express';
 
 const passwordRoutes = Router();
 
 const forgotPasswordMailController = new ForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 passwordRoutes.post('/forgot', forgotPasswordMailController.handle);
+passwordRoutes.post('/reset', resetPasswordUserController.handle);
 
 export { passwordRoutes };
