b5a20382b12d992490f2f05fb365a177dcc1c207
Victor Mello <victor.mello@al.infnet.edu.br>
2022-03-27 22:04:57 -0300

    Incluído secret key no provider do SES para evitar erros de credenciais pendentes

--- src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -19,6 +19,8 @@ class SESMailProvider implements IMailProvider {
         SES: new SES({
           apiVersion: '2010-12-01',
           region: process.env.AWS_REGION,
+          accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+          secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID,
         }),
       });
     } catch (err) {
