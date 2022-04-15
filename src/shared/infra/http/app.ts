69282010907e8dcfe753f20ba0514de41da659f2
Victor Mello <victorsmello93@gmail.com>
2022-04-15 18:52:41 -0300

    Implementado o middleware que trata o número de requisições para evitar ataques DDoS com Redis e Rate Limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -10,6 +10,7 @@ import cors from 'cors';
 import { router } from './routes';
 import swaggerFile from '../../../swagger.json';
 import upload from '../../../config/upload';
+import rateLimiter from './middlewares/rateLimiter';
 
 createConnection();
 
@@ -18,6 +19,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors());
+app.use(rateLimiter);
 
 app.use(router);
 
